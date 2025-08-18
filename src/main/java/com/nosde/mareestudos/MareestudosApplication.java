1830af21bc55c10ce5705003d751e0bdab729abb
edsonarauj0 <edsonaraujo.dev@gmail.com>
2025-08-18 06:28:44 -0300

    first commit

---
+++ src/main/java/com/nosde/mareestudos/MareestudosApplication.java
@@ -0,0 +1,13 @@
+package com.nosde.mareestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MareestudosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MareestudosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nosde/mareestudos/MareestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nosde.mareestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MareestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
